3a9af3b8141fcb02523450ed363f1afe3541a43e
yejinstar <gyj5586@gmail.com>
2022-10-28 15:26:45 +0900

    UserController /user POST, DELETE

--- src/main/java/com/springboot/hello/controller/UserController.java
+++ src/main/java/com/springboot/hello/controller/UserController.java
@@ -1,22 +1,34 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.dao.UserDao;
+import com.springboot.hello.domain.User;
+import com.springboot.hello.domain.dto.UserRequestDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/vi/user")
 public class UserController {
 
-    private final UserDao userDao;
+    //    private final UserDao userDao;
+    @Autowired
+    private UserDao userDao;
+
 
     public UserController(UserDao userDao) {
         this.userDao = userDao;
     }
 
-    @DeleteMapping("/user")
+    @PostMapping("/")
+    public ResponseEntity<Integer> add(@RequestBody UserRequestDto userRequestDto) {
+        User user = new User(userRequestDto.getId(), userRequestDto.getName(),
+                userRequestDto.getPassword());
+        return ResponseEntity
+                .ok()
+                .body(userDao.add(user));
+    }
+    @DeleteMapping("/")
     public ResponseEntity<Integer> deleteAll() {
         return ResponseEntity
                 .ok()
