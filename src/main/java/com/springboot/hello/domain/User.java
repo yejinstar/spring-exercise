dd215cd6c3858bc4ee7d3008842cdaae87e075e8
yejinstar <gyj5586@gmail.com>
2022-10-28 14:03:02 +0900

    User, UserDao 추가

---
+++ src/main/java/com/springboot/hello/domain/User.java
@@ -0,0 +1,28 @@
+package com.springboot.hello.domain;
+
+import lombok.Setter;
+
+@Setter
+public class User {
+    private String id;
+    private String name;
+    private String password;
+
+    public User(String id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
