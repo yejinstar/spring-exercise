512eddba943a38bb95c56d8cd48ac3ad4dc8e26a
yejinstar <gyj5586@gmail.com>
2022-10-28 11:34:52 +0900

    @Slf4j로 log남기기

--- src/main/java/com/springboot/hello/controller/GetController.java
+++ src/main/java/com/springboot/hello/controller/GetController.java
@@ -1,6 +1,7 @@
 package com.springboot.hello.controller;
 
 import com.springboot.hello.dto.MemberDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -8,19 +9,23 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
+@Slf4j
 public class GetController {
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
+        log.info("hello로 요청이 들어왔습니다.");
         return "Hello World";
     }
 
     @GetMapping(value = "/name")
     public String getName(){
+        log.info("getName으로 요청이 들어왔습니다.");
         return "Flature";
     }
 
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable("variable") String variable){
+        log.info("getVariable1로 요청이 들어왔습니다. variable:{}", variable); //http://localhost:8080/api/v1/get-api/variable1/variable1
         return variable;
     }
 
