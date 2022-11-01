02150793019906312e6d5f7b29aa0c717d31dfec
yejinstar <gyj5586@gmail.com>
2022-11-01 16:18:53 +0900

    HospitalDao add() 수정

--- src/main/java/com/springboot/hello/dao/HospitalDao.java
+++ src/main/java/com/springboot/hello/dao/HospitalDao.java
@@ -3,6 +3,7 @@ package com.springboot.hello.dao;
 import com.springboot.hello.domain.Hospital;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 import javax.sql.DataSource;
 
@@ -27,6 +28,36 @@ public class HospitalDao {
                 hospital.getHealthcareProviderCount(), hospital.getPatientRoomCount(), hospital.getTotalNumberOfBeds(),
                 hospital.getTotalAreaSize()
         );
+    }
+    RowMapper<Hospital> rowMapper = (rs, rowNum) -> {
+        Hospital hospital = new Hospital();
+        hospital.setId(rs.getInt("id"));
+        hospital.setOpenServiceName(rs.getString("open_service_name"));
+        hospital.setManagementNumber(rs.getString("management_number"));
+        hospital.setOpenLocalGovernmentCode(rs.getInt("open_local_government_code"));
+        hospital.setLicenseDate(rs.getTimestamp("license_date").toLocalDateTime());
+        hospital.setBusinessStatus(rs.getInt("business_status"));
+        hospital.setBusinessStatusCode(rs.getInt("business_status_code"));
+        hospital.setPhone(rs.getString("phone"));
+        hospital.setFullAddress(rs.getString("full_address"));
+        hospital.setRoadNameAddress(rs.getString("road_name_address"));
+        hospital.setHospitalName(rs.getString("hospital_name"));
+        hospital.setBusinessTypeName(rs.getString("business_type_name"));
+        hospital.setHealthcareProviderCount(rs.getInt("healthcare_provider_count"));
+        hospital.setPatientRoomCount(rs.getInt("patient_room_count"));
+        hospital.setTotalNumberOfBeds(rs.getInt("total_number_of_beds"));
+        hospital.setTotalAreaSize(rs.getFloat("total_area_size"));
+
+        return hospital;
+    };
+
+    public Hospital findById(int id){
+        return this.jdbcTemplate.queryForObject("SELECT * FROM nation_wide_hospitals " +
+                "WHERE id = ?", rowMapper, id);
+    }
+
+    public void deleteAll(){
+        this.jdbcTemplate.update("DELETE FROM nation_wide_hospitals");
     }
 
     public int getCount(){
