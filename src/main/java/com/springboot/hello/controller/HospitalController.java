9f620f581a886d8713299ef64171bb24a1311f07
yejinstar <gyj5586@gmail.com>
2022-11-02 13:17:09 +0900

    HospitalController Optional 사용해서 1차 데이터 불러오기

--- src/main/java/com/springboot/hello/controller/HospitalController.java
+++ src/main/java/com/springboot/hello/controller/HospitalController.java
@@ -3,11 +3,14 @@ package com.springboot.hello.controller;
 import com.springboot.hello.dao.HospitalDao;
 import com.springboot.hello.domain.Hospital;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/vi/hospital")
@@ -20,9 +23,18 @@ public class HospitalController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Hospital> getById(@PathVariable("id") int id) {
-        return ResponseEntity
+    public ResponseEntity<Hospital> getById(@PathVariable("id") Integer id) {
+        /*return ResponseEntity
                 .ok()
-                .body(hospitalDao.findById(id));
+                .body(hospitalDao.findById(id));*/
+
+        Hospital hospital = hospitalDao.findById(id);
+        Optional<Hospital> opt = Optional.of(hospital);
+
+        if (!opt.isEmpty()) {
+            return ResponseEntity.ok().body(hospital);
+        } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new Hospital());
+        }
     }
 }
